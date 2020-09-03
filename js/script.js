c95e6f55518d3c66cc4101ce925ab06e24cffa5d
Sam Spencer-McFadden <45200568+SSpencer-Mcfad99@users.noreply.github.com>
2020-09-03 14:07:00 +0100

    Added contactme to about me and added social links

--- js/script.js
+++ js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
   $('.hamburger').click(function(event) {
      $('.nav-links').toggleClass('displayed');
    });
+
+   $('.mail').click(function(event) {
+     window.location.href = "mailto:sam.mcfadden2@gmail.com";
+   })
+
+   $('.linkedin').click(function(event) {
+      window.location.href = "https://www.linkedin.com/in/sam-spencer-mcfadden-a57a42152/";
+   })
 });
